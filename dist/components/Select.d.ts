901d75cdac8269389a8119290f90cc811871501f
duynguyenJRGVN <duy.nguyen@jrgvn.com>
2025-03-07 21:54:55 +0700

    * update search include // Import hàm removeVietnameseTones

---
+++ dist/components/Select.d.ts
@@ -0,0 +1,5 @@
+import React from "react";
+import { SelectProps } from "./type";
+export declare const removeVietnameseTones: (str: string) => string;
+declare const Select: React.FC<SelectProps>;
+export default Select;
